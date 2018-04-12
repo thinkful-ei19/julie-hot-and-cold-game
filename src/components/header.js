8a490d2e9b24509e6451d8684c7883541a3b3e7a
Julie K <julieskim160@gmail.com>
2018-04-12 08:50:11 -0700

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import TopNav from './top-nav';
 import InfoModal from './info-modal';
 
 import './header.css';
 
-export default function Header(props) {
-    return (
-        <header>
-            <TopNav />
-            {/* <InfoModal /> */}
-            <h1>HOT or COLD</h1>
-        </header>
-    );
-};
+export default class Header extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            showWhat: false
+        }
+    }
+    render() {
+        return (
+            <header>
+                <TopNav />
+                <InfoModal />
+                <h1>HOT or COLD</h1>
+            </header>
+        );
+    };
+
+
+
+}
